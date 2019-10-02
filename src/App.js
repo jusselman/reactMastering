38f075077c0ee07f3ec054901b03dbac7b632be0
jusselman <usselmanjosh@gmail.com>
2019-10-01 21:21:51 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import GameBoard from './components/GameBoard/GameBoard';
+import ColorPicker from './components/ColorPicker/ColorPicker';
+import GameTimer from './components/GameTimer/GameTimer';
+import GuessPeg from './components/GuessPeg/GuessPeg';
+import GuessPegs from './components/GuessPegs/GuessPegs';
+import NewGameButton from './components/NewGameButton/NewGameButton';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const colors = ['#7CCCE5', '#FDE47F', '#E04644', '#B576AD'];
+
+
+class App extends Component {
+  constructor() {
+    super();
+    // state is an object that
+    // the info for our app
+    this.state = {
+      selColorIdx: 0,
+      guesses: [this.getNewGuess(), this.getNewGuess()],
+      code: this.genCode()
+    };
+  }
+
+  genCode() {
+    return new Array(4).fill().map(() => Math.floor(Math.random() * colors.length));
+  }
+
+  getNewGuess() {
+    return {
+      // code: [null, null, null, null],
+      code: [3, 2, 1, 0],
+      score: {
+        perfect: 0,
+        almost: 0
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={() =>
+          this.setState((state) => ({ selColorIdx: ++this.state.selColorIdx % 4 }))
+        }>
+          Next Color
+          </button>
+
+        Selected color: {colors[this.state.selColorIdx]}
+
+        <header className="App-header">
+          React Mastering
+        </header>
+        <div className="flex-h">
+          <GameBoard
+            colors={colors}
+            guesses={this.state.guesses}
+          />
+          <div>
+            <ColorPicker
+              colors={colors}
+              selColorIdx={this.state.selColorIdx}
+            />
+            <GameTimer />
+            <NewGameButton />
+          </div>
+        </div>
+        <footer>Footer</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/GuessPegs/GuessPegs.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import GuessPeg from '../GuessPeg/GuessPeg';
+
+const GuessPegs = (props) => (
+    <div className='flex-h'>
+        <GuessPeg
+            color={props.colors[props.code[0]]}
+        />
+        <GuessPeg
+            color={props.colors[props.code[1]]}
+        />
+        <GuessPeg
+            color={props.colors[props.code[2]]}
+        />
+        <GuessPeg
+            color={props.colors[props.code[3]]}
+        />
+    </div>
+);
+
+export default GuessPegs
